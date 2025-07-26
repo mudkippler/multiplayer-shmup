2698faa95225c0aafcde097e927ac3c308875587
Samir Chaoul <tehraiden@gmail.com>
2025-07-25 23:27:24 -0400

    update start message

--- server.js
+++ server.js
@@ -136,5 +136,5 @@ setInterval(gameLoop, 1000 / TICK_RATE);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
